8a7e36668e2072055fe8c0333d9225cbe30a1c1f
Lex Nelson <lexinelson597@gmail.com>
2021-07-22 09:56:17 -0400

    commit tree c6805914c383a008a5b159e9c7a893be2041bfc8

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, {useState} from "react";
+
+
 
 function Item({ name, category }) {
+  const [isOn, setIsOn]= useState(false)
+  function handleClick(){
+    setIsOn(isOn => !isOn)
+  }
+  
+  const inCart = isOn ? "in-cart" : ""
+  const btnLabel = isOn ? "Remove From Cart" : "Add to Cart"
+ 
   return (
-    <li className="">
+    <li className={inCart}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className="add" onClick = {handleClick}>{btnLabel}</button>
     </li>
   );
 }
